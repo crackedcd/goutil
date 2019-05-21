8f4e48d3e5eac3996d9bf82d96c4eba4cd95859a
cc <crackedcd@qq.com>
2019-05-21 10:54:54 +0800

    修改GetExecPath()函数为golang推荐方法.

--- common/utils/fileutil/pathutil.go
+++ common/utils/fileutil/pathutil.go
@@ -1,19 +1,21 @@
 package fileutil
 
 import (
-    "os"
-    "path/filepath"
-    "runtime"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 func GetExecPath() string {
-    dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-    return dir
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Dir(ex)
 }
 
 func GetFilePath() string {
-    // runtime.Caller()用户获取栈帧的信息, 参数为0表示当前函数, 参数为1表示上层调用该函数的函数.
-    _, filename, _, _ := runtime.Caller(1)
-    return filepath.Dir(filename)
+	// runtime.Caller()用户获取栈帧的信息, 参数为0表示当前函数, 参数为1表示上层调用该函数的函数.
+	_, filename, _, _ := runtime.Caller(1)
+	return filepath.Dir(filename)
 }
-
